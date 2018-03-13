c4ba2a2e8bbc8487d3e9e8cac785e6fbed6aa726
Wali Rahman <wali.rahman89@yahoo.com>
2018-03-13 10:43:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,14 @@ class Student
   end
 
   def create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
 
+    DB[:conn].execute(sql)
   end
 end
