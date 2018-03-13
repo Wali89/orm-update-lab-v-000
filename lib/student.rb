a27e49e63c04d0aefa455743cd86b2fa7dbcecfc
Wali Rahman <wali.rahman89@yahoo.com>
2018-03-13 11:46:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,4 +67,10 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def update
+    sql = "UPDATE songs Set name = ?, album = ? wWHERE id = ?"
+
+  end
+
 end
